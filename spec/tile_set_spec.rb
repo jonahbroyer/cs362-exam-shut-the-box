77b8402730dcb485af4b0bccb1efd53389f7b7aa
Jonah Broyer <jkbsk8er@yahoo.com>
2022-03-17 19:33:18 -0700

    Tried completing tile_set tests

--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -2,19 +2,20 @@ require_relative '../lib/tile_set'
 
 describe 'A tile set' do
 
+  let (:tile_set) { TileSet.new }
+
   describe '::generate' do
 
     it 'returns an array of 9 tiles by default' do
-      tile_set = TileSet.new
-      expect(tile_set).to eq(9)
+      expect(tile_set.generate).to eq(9)
     end
 
     it 'returns an array of tiles of a specified length' do
-      skip
+      expect(tile_set.generate(4)).to eq(4)
     end
 
     it 'returns an array of tiles whose values increase from 1, by 1' do
-      skip
+      expect(tile_set.generate).to eq(9)
     end
 
   end
