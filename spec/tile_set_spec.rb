38acbc9464f2bd2475f72f4ac2f76692880de49a
Jonah Broyer <jkbsk8er@yahoo.com>
2022-03-17 19:25:04 -0700

    Implemented first test

--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -5,7 +5,8 @@ describe 'A tile set' do
   describe '::generate' do
 
     it 'returns an array of 9 tiles by default' do
-      raise "Start here"
+      tile_set = TileSet.new
+      expect(tile_set).to eq(9)
     end
 
     it 'returns an array of tiles of a specified length' do
